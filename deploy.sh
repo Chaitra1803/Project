e7bcf8836f43c87b79edf2fce8496550effdf66f
Chaitra Teggi <48706140+Chaitra1803@users.noreply.github.com>
2023-11-07 20:39:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,14 @@
 
 if (["$GIT_BRANCH" == 'origin/dev' ]) then
     build.sh
-    docker login
-    withCredentials([usernamePassword(credentialsId: "${DOCKER_REGISTRY_CREDS}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin docker.io"
-        sh 'docker push $DOCKER_BFLASK_IMAGE'
-    }
+    docker login -u veera1808 dckr_pat_C7FMRRRiGBfoZvetq6DDyyYf8OU
     docker tag my-img $DOCKER_BFLASK_IMAGE
     docker push $DOCKERHUB_PUBLICREPO
 
 
 elif ([ "$GIT_BRANCH" == 'origin/main' ]) then
     build.sh
-    docker login
-    withCredentials([usernamePassword(credentialsId: "${DOCKER_REGISTRY_CREDS}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin docker.io"
-        sh 'docker push $DOCKER_BFLASK_IMAGE'
-    }
+    docker login -u veera1808 dckr_pat_C7FMRRRiGBfoZvetq6DDyyYf8OU
     docker tag my-img $DOCKER_BFLASK_IMAGE
     docker push $DOCKERHUB_PRIVATEREPO
 
