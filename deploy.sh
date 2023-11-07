9a186ade7ea25c57c56b0d94195c17e0c57785c4
Chaitra Teggi <48706140+Chaitra1803@users.noreply.github.com>
2023-11-07 19:15:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if (["$GIT_BRANCH" == 'origin/dev' ]) then
-    # Build and push to the development repository
     build.sh
     docker login
     withCredentials([usernamePassword(credentialsId: "${DOCKER_REGISTRY_CREDS}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
@@ -13,7 +12,6 @@ if (["$GIT_BRANCH" == 'origin/dev' ]) then
 
 
 elif ([ "$GIT_BRANCH" == 'origin/main' ]) then
-    # Build and push to the production repository
     build.sh
     docker login
     withCredentials([usernamePassword(credentialsId: "${DOCKER_REGISTRY_CREDS}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
