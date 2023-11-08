cf90568416f860216dc8f383eb82161b288c6c80
Chaitra Teggi <48706140+Chaitra1803@users.noreply.github.com>
2023-11-08 11:48:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,15 @@
 if (["$GIT_BRANCH" == 'origin/dev' ]) then
     build.sh
     docker login -u veera1808 dckr_pat_C7FMRRRiGBfoZvetq6DDyyYf8OU
-    docker tag my-img:$TAG $DOCKER_BFLASK_IMAGE:$TAG
-    docker push $DOCKERHUB_PUBLICREPO:$TAG
+    docker tag react-appimg veera1808/dev_repo
+    docker push veera1808/dev_repo
 
 
 elif ([ "$GIT_BRANCH" == 'origin/main' ]) then
     build.sh
     docker login -u veera1808 dckr_pat_C7FMRRRiGBfoZvetq6DDyyYf8OU
-    docker tag my-img:$TAG $DOCKER_BFLASK_IMAGE:$TAG
-    docker push $DOCKERHUB_PRIVATEREPO:$TAG
-
+    docker tag react-appimg veera1808/prod_repo
+    docker push veera1808/prod_repo
 
 
    
